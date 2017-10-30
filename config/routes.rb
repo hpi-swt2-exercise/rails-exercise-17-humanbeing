d08e801bc7fd117989d8c73e746e8d9ec3c435ee
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-10-30 15:48:41 +0100

    test runs through. issue came back anyway. modifing random stuff until i get a new issue. Closes #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'new/author' => 'authors#new'
+  get 'authors/new'
+
+  #get 'new/author' => 'authors#new'
+  # this worked for some reason
+  
+  #get 'authors/new' => 'authors#new'
+  resources :authors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
