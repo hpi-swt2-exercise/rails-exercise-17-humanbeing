fb645a08839f43da840430ea4e975498358d61c1
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-05 15:25:11 +0100

    commented in the migration. Closes #6

--- db/migrate/20171105134705_create_authors.rb
+++ db/migrate/20171105134705_create_authors.rb
@@ -1,12 +1,11 @@
 class CreateAuthors < ActiveRecord::Migration
-#  def change
-#    create_table :authors do |t|
-#      t.string :first_name
-#      t.string :last_name
-#      t.string :name
-#      t.string :homepage
+  def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :homepage
 
-#      t.timestamps null: false
-#    end
-#  end
+      t.timestamps null: false
+    end
+  end
 end
