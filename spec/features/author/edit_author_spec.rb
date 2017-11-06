e66798e9f73aa78914fb9a9bd7138ac103b29333
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-06 03:33:15 +0100

    Failing test for #13

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "edit for author page", type: :feature do
+  
+  
+  it "should edit without error" do
+	expect{
+		author = Author.new(first_name:'Alan', last_name: 'Turing', homepage: 'www.adresse.de')
+		author.save
+		
+		visit edit_author_path(author)
+		fill_in('First name', :with => 'Alan Mathison')
+		click_button('Save Author')	
+	}.not_to raise_error
+  end
+end
