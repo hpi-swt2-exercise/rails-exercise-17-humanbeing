578cf4bf034511a7a390c7298c5ac03e3f90652c
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-06 02:13:36 +0100

    Failing Test for #8

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -13,4 +13,12 @@ describe "index for author page", type: :feature do
 	
 	expect(page).to have_link('Add author')
   end
+  
+  it "should have a link to show alan turing" do
+	Author.new(first_name:'Alan', last_name: 'Turing', homepage: 'www.adresse.de').save
+	
+	visit authors_path
+	
+	expect(page).to have_link('Show')
+  end
 end
