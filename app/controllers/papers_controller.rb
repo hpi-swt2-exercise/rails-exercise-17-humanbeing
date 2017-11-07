24e7afc6d94d36af7db297c86969c4a135983630
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-07 16:29:46 +0100

    refactoring

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,2 +1,12 @@
 class PapersController < ApplicationController
+  def create
+    @author = Author.find(params[:author_id])
+    @paper = @author.papers.create(paper_params)
+    redirect_to author_path(@author)
+  end
+ 
+  private
+    def paper_params
+      params.require(:paper).permit(:title, :venue, :year)
+    end
 end
