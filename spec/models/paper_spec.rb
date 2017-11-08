cd0b7b13e0c47b1f70898ca55c5e879ab1385b04
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-08 20:32:15 +0100

    Failing test for #17

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -4,4 +4,8 @@ require 'rails_helper'
     it "should validate that there is a title" do
 	  expect(Paper.new(title: '', venue: '', year: '').save).to eq(false)
     end
+	
+	it "should validate that there is a venue" do
+	  expect(Paper.new(title: '', venue: '', year: '').save).to eq(false)
+    end
   end
