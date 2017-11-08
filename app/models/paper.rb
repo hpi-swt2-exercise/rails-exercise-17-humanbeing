e5f2495ef5cb94bc13bb64d0fbad3030e0196415
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-08 20:57:47 +0100

    added validation. Closes #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,5 @@ class Paper < ActiveRecord::Base
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true
+  validates :year, numericality: { only_integer: true }
 end
