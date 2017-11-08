9756bf300a4365e8bf26294f069af0902ad7985b
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-08 21:41:00 +0100

    Failing test for #22

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,4 +5,11 @@ describe "New paper page", type: :feature do
   it "should render withour error" do
     visit new_paper_path
   end
+  
+  it "should show error massage when there is no title" do
+	visit new_paper_path
+	click_button('Save Paper')
+	
+	expect(page).to have_text('Title can\'t be blank')
+  end
 end
