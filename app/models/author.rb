297ea122d653cf8cf2d2d6300c88bb269511335e
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-09 00:02:00 +0100

    refactoring

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
 	def name
-		first_name + " " + last_name
+		name = first_name + " " + last_name
 	end
 	has_and_belongs_to_many :papers
 	validates :last_name, presence: true
