0bcdc64f1441bfbcff43dc89f5716f50864c5fae
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-09 09:10:07 +0100

    hoepfully fixed bug

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
 	def name
-		name = first_name + " " + last_name
+		first_name + " " + last_name
 	end
 	has_and_belongs_to_many :papers
 	validates :last_name, presence: true
