6f8b0877aed19dda50d68b7b296c7dc3b5621a7e
Michael Vaichenker <Michael.Vaichenker@student.hpi.de>
2017-11-09 23:23:57 +0100

    Failing test for #31

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -26,4 +26,12 @@ describe "edit for paper page", type: :feature do
 	visit edit_paper_path(paper)
 	expect(page).to have_content("Author 1")
   end
+
+  it "should preselect author" do
+	paper = Paper.new(title:'Alan', venue: 'Turing', year: 1234)
+	paper.save
+	author = Author.new(first_name: 'Horst', last_name: 'Horstinger', homepage: 'www.horst.de')
+	visit edit_paper_path(paper)
+	expect(page).not_to have_content("none")
+  end
 end
